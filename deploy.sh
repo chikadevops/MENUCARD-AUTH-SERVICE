f1defc52cc2caa640f009bdb84ed4e0623c00fc0
Chikadevops <chikadevops@gmail.com>
2025-04-17 22:15:30 +0100

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,4 @@ npm install --save-dev jest
 npm test
 
 # Run App
-npm run dev &
-DEV_PID=$!
-echo "Dev server running with PID $DEV_PID"
-sleep 60
-kill -- -$DEV_PID
-echo "Dev server stopped after 1 minute"
+timeout --foreground 60s npm run dev || echo "Stopped dev server after timeout"
